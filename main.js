5c3cad4185da836157c4fb2dd2734dd9d2e1fe09
ASHLY <157629868+Ashly565@users.noreply.github.com>
2024-07-14 21:50:09 +0530

    update

--- main.js
+++ main.js
@@ -3,17 +3,20 @@ let btn=document.getElementById('btn');
 btn.addEventListener('click',function(){
     let weight=document.getElementById('weight-input').value;
     let height=document.getElementById('height-input').value;
+    let results=document.getElementById('result')
+
     if (height <= 0 || weight <= 0 || isNaN(height) || isNaN(weight)) {
         alert('Please fill the fields ');
         return;
     }
-    let bmi=(weight/(height*height)*10000);
-    document.getElementById('bmi-output').value=bmi;
+    let bmi=(weight/(height*height)*10000).toFixed(2);
+    document.getElementById('btn').value=bmi;
+
     if(bmi>25){
-        document.getElementById('hlt-output').value="Over-Weight";
+       results.innerHTML= bmi + "-Over-weight"
     }else if(bmi<18){
-        document.getElementById('hlt-output').value="Under-Weight";
+         results.innerHTML= bmi + " -NORMAL"
     }else{
-        document.getElementById('hlt-output').value="Normal";
+         results.innerHTML= bmi + "-Under-weight"
     }
 })
